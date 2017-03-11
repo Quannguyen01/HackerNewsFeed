fb023c236e690f865a0b5eba448d776202ff4d19
quannguyen01 <quannguyenhong.92@gmail.com>
2017-03-10 19:49:15 -0600

    Add React, not happy about it

---
+++ app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>Hacker News</h1>
+        <MainContainer/>
+      </div>
+    );
+  }
+}
+
+export default App;
--- app/index.js
+++ app/index.js
@@ -1,45 +1,56 @@
-import {shell} from 'electron';
-import {fetchTopStories, fetchStory} from './HNHelpers.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const wrapper = document.querySelector(".wrapper");
-const board = wrapper.querySelector("#board");
+import App from './App';
 
-function display(stories) {
+// import {shell} from 'electron';
+// import {fetchTopStories, fetchStory} from './HNHelpers.js';
 
-  let list = '';
-  stories.forEach(story => {
-    const content = `
-      <div class="story">
-        <a href="${story.url}">${story.title}</a> by ${story.by}
-      </div>
-    `;
-    // const content = `
-    //   <some-custom-element ${story.url} ${story.title} ${story.by}>
-    // `;
-    list = list + content;
-  });
+// const wrapper = document.querySelector(".wrapper");
+// const board = wrapper.querySelector("#board");
 
-  board.innerHTML = list;
+// function display(stories) {
+//
+//   let list = '';
+//   stories.forEach(story => {
+//     const content = `
+//       <div class="story">
+//         <a href="${story.url}">${story.title}</a> by ${story.by}
+//       </div>
+//     `;
+//     // const content = `
+//     //   <some-custom-element ${story.url} ${story.title} ${story.by}>
+//     // `;
+//     list = list + content;
+//   });
+//
+//   board.innerHTML = list;
+//
+//   const storiesDOM = document.querySelectorAll(".story");
+//   storiesDOM.forEach(story => story.addEventListener('click', openLink));
+// }
+//
+// function openLink(e){
+//   e.preventDefault();
+//   shell.openExternal(this.querySelector('a').href);
+// }
+//
+// function showStories() {
+//
+//   board.innerHTML = "Loading...";
+//
+//   fetchTopStories()
+//   .then(data => {
+//     if (data.length > 0){
+//         display(data);
+//     }
+//   });
+// }
+//
+// window.onload = showStories;
 
-  const storiesDOM = document.querySelectorAll(".story");
-  storiesDOM.forEach(story => story.addEventListener('click', openLink));
-}
 
-function openLink(e){
-  e.preventDefault();
-  shell.openExternal(this.querySelector('a').href);
-}
-
-function showStories() {
-
-  board.innerHTML = "Loading...";
-
-  fetchTopStories()
-  .then(data => {
-    if (data.length > 0){
-        display(data);
-    }
-  });
-}
-
-window.onload = showStories;
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+)
