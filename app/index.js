a0c62eaa2b8ce4eeb5b2dac3cc590eb1424b6d4c
quannguyen01 <quannguyenhong.92@gmail.com>
2017-03-11 19:33:17 -0600

    Removes comments

--- app/index.js
+++ app/index.js
@@ -3,53 +3,6 @@ import ReactDOM from 'react-dom';
 
 import MainContainer from './containers/MainContainer';
 
-// import {shell} from 'electron';
-// import {fetchTopStories, fetchStory} from './HNHelpers.js';
-
-// const wrapper = document.querySelector(".wrapper");
-// const board = wrapper.querySelector("#board");
-
-// function display(stories) {
-//
-//   let list = '';
-//   stories.forEach(story => {
-//     const content = `
-//       <div class="story">
-//         <a href="${story.url}">${story.title}</a> by ${story.by}
-//       </div>
-//     `;
-//     // const content = `
-//     //   <some-custom-element ${story.url} ${story.title} ${story.by}>
-//     // `;
-//     list = list + content;
-//   });
-//
-//   board.innerHTML = list;
-//
-//   const storiesDOM = document.querySelectorAll(".story");
-//   storiesDOM.forEach(story => story.addEventListener('click', openLink));
-// }
-//
-// function openLink(e){
-//   e.preventDefault();
-//   shell.openExternal(this.querySelector('a').href);
-// }
-//
-// function showStories() {
-//
-//   board.innerHTML = "Loading...";
-//
-//   fetchTopStories()
-//   .then(data => {
-//     if (data.length > 0){
-//         display(data);
-//     }
-//   });
-// }
-//
-// window.onload = showStories;
-
-
 ReactDOM.render(
   <MainContainer/>,
   document.getElementById('app')
