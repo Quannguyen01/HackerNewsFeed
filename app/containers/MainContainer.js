b8da552c93a8efebd5a3805f2969633d16de5043
quannguyen01 <quannguyenhong.92@gmail.com>
2017-03-14 00:28:16 -0500

    Adds close and refresh buttons

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { remote } from 'electron';
 
 import Stories from '../components/Stories';
 
@@ -14,6 +15,24 @@ class MainContainer extends Component {
     }
   }
 
+  closeApp() {
+    remote.getCurrentWindow().close();
+  }
+
+  fetch() {
+    this.setState({
+      isLoading: true,
+    });
+
+    fetchTopStories()
+      .then(data => {
+        this.setState({
+          isLoading: false,
+          stories: data
+        })
+      });
+  }
+
   componentDidMount() {
     fetchTopStories()
       .then(data => {
@@ -27,12 +46,25 @@ class MainContainer extends Component {
   render() {
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <header className="header mdl-layout__header">
-          <span className="mdl-layout-title">Hacker News</span>
+        <header className="mdl-layout__header">
+          <div className="header mdl-layout__header-row">
+            <span className="mdl-layout-title">Hacker News</span>
+            <div className="mdl-layout-spacer"></div>
+            <div className="mdl-button mdl-js-button mdl-button--icon" onClick={this.fetch.bind(this)}>
+              <i className="material-icons">refresh</i>
+            </div>
+            <div className="mdl-button mdl-js-button mdl-button--icon" onClick={this.closeApp}>
+              <i className="material-icons">close</i>
+            </div>
+          </div>
         </header>
-        <Stories
-          isLoading={this.state.isLoading}
-          stories={this.state.stories}/>
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            <Stories
+              isLoading={this.state.isLoading}
+              stories={this.state.stories}/>
+          </div>
+        </main>
       </div>
     );
   }
